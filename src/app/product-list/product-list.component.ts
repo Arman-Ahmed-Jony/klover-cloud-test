d879d23d25610ded24533579488ce1c5d3af972d
arman-ahmed-jony <armanahmedjony786@gmail.com>
2021-01-16 22:05:05 +0600

    added product click event

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,27 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { ProdcutsService } from '../services/prodcuts.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   closeResult = '';
-  products:any = [];
+  products: any = [];
 
-  constructor(private modalService: NgbModal, private productService: ProdcutsService) {
-  }
+  constructor(
+    private modalService: NgbModal,
+    private productService: ProdcutsService
+  ) {}
 
   open(content: any) {
-    console.log(content)
-    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', centered: true, size: 'lg' }).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
+    console.log(content);
+    this.modalService
+      .open(content, {
+        ariaLabelledBy: 'modal-basic-title',
+        centered: true,
+        size: 'lg',
+      })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
   }
 
   private getDismissReason(reason: any): string {
@@ -33,17 +43,25 @@ export class ProductListComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
-  
+
   getAllProduct() {
-    this.productService.getAllProduct().subscribe(data => {
-      console.log(data)
-    }, error => {})
+    this.productService.getAllProduct().subscribe(
+      (data) => {
+        console.log(data);
+      },
+      (error) => {}
+    );
+  }
+  showProductInfo(item: any) {
+    console.log(item);
   }
   ngOnInit(): void {
-    this.productService.getAllProduct().subscribe(data => {
-      console.log(data)
-      this.products = data
-    }, error => {})
+    this.productService.getAllProduct().subscribe(
+      (data) => {
+        console.log(data);
+        this.products = data;
+      },
+      (error) => {}
+    );
   }
-
 }
